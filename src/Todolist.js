6af186c4cf61cd2029289e5d53ddf68751c6cc77
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-05-03 22:16:40 -0400

    update

--- src/Todolist.js
+++ src/Todolist.js
@@ -3,7 +3,14 @@ import React, {Component  } from "react";
 class Todolist extends Component{
   render() {
     return (
+      <div>
       <div><input/><button>Submit</button></div>
+      <ul>
+        <li>Learning React</li>
+        <li>Work Out</li>
+        <li>Finish Project</li>
+      </ul>
+      </div>
     )
   }
 }
